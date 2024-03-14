73e3dd2d4f8088b292de2189a26535abbc2ab50e
Koekieezz <112951868+Koekieezz@users.noreply.github.com>
2024-03-14 19:47:27 +0700

    replace all

--- src/main/java/me/machinemaker/treasuremapsplus/loot/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Handles overriding the codec for the {@link net.minecraft.world.level.storage.loot.functions.ExplorationMapFunction}
- */
-@DefaultQualifier(NonNull.class)
-package me.machinemaker.treasuremapsplus.loot;
-
-import org.checkerframework.checker.nullness.qual.NonNull;
-import org.checkerframework.framework.qual.DefaultQualifier;
